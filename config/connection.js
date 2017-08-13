75ba55b03d8cb818ea5168b1a649d17a816cd6a9
Saveliy <samoylov3@hotmail.com>
2017-08-13 02:26:45 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql")
 
-let connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'burgers_db'
-})
+
+
+if (process.env.JAWSDB_URL){
+  let connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+  })
+}
 
 connection.connect((err)=>{
   if (err) {
